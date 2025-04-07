22d7d67dd9b7bfc349425b4573c1bd0e8597e03e
retnuh <98132919+retnuh-code@users.noreply.github.com>
2025-04-07 18:41:14 -0400

    Update vite.config.js

--- LibreShelf/frontend/vite.config.js
+++ LibreShelf/frontend/vite.config.js
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: true, // Accept all external connections
     port: 3000,
     strictPort: true,
     cors: true,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+    },
   },
   preview: {
     host: true,
@@ -16,10 +20,6 @@ export default defineConfig({
     cors: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
-    },
-    // This disables hostname checking altogether
-    proxy: {},
-    // Add this to remove host filtering
-    allowedHosts: ['*'],
-  },
+    }
+  }
 });
