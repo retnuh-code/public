ad5e3de3dc18ec0dda9ae1af7d4124233dae171a
retnuh-code <hunterhearne@gmail.com>
2025-04-08 00:01:40 -0400

    updated

--- LibreShelf/frontend/src/main.jsx
+++ LibreShelf/frontend/src/main.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles.css'
-import './App.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './App.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
