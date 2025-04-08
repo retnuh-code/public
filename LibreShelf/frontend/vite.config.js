f161add7c53e01b9cb33561db0ed4a84e2db45fa
retnuh <98132919+retnuh-code@users.noreply.github.com>
2025-04-07 20:12:03 -0400

    Update vite.config.js

--- LibreShelf/frontend/vite.config.js
+++ LibreShelf/frontend/vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: 3000,
-    host: true,
-    strictPort: true
-  },
-  server: {
-    host: true
+    host: true, // <- allow all hosts
+    port: 3000
   }
 });
