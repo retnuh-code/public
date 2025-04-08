ab8202719b94ca22216503e26fe8407c67b3f0ac
retnuh-code <hunterhearne@gmail.com>
2025-04-08 13:44:11 -0400

    update

--- LibreShelf/frontend/vite.config.js
+++ LibreShelf/frontend/vite.config.js
@@ -10,8 +10,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://host.docker.internal:4000',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
+        changeOrigin: true
       }
     }
   }
