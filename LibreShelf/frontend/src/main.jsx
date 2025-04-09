30219a51efb8bf35a9de9cfaa14ff09cf33556a7
retnuh-code <hunterhearne@gmail.com>
2025-04-08 22:56:05 -0400

    .

--- LibreShelf/frontend/src/main.jsx
+++ LibreShelf/frontend/src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './assets/index.css'
+
+// 🛠 Manually polyfill process.nextTick
+if (typeof process === 'undefined') {
+  window.process = {}
+}
+if (typeof process.nextTick === 'undefined') {
+  process.nextTick = function (cb) {
+    Promise.resolve().then(cb)
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
