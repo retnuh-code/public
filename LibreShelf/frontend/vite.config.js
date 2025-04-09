c07aee5199f516dc1939457a576466db3f31262d
retnuh-code <hunterhearne@gmail.com>
2025-04-08 22:40:05 -0400

    fix process

--- LibreShelf/frontend/vite.config.js
+++ LibreShelf/frontend/vite.config.js
@@ -1,16 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
 export default defineConfig({
   define: {
-    'process.env': {}
+    global: 'globalThis',
+    'process.env': {},
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      process: 'rollup-plugin-polyfill-node/polyfills/process-es6',
+    },
+  },
+  optimizeDeps: {
+    include: ['process'],
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 3000
