a9c3660a10e82e700cf6f923f3f1dcaa54539fb0
retnuh-code <hunterhearne@gmail.com>
2025-04-08 23:56:28 -0400

    .

--- LibreShelf/frontend/vite.config.js
+++ LibreShelf/frontend/vite.config.js
@@ -1,8 +1,10 @@
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
   server: {
     host: true
   }
-};
+})
