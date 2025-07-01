82dae9a33a8fc5e9a65b4b06a98cff4f61b407dd
retnuh <98132919+retnuh-code@users.noreply.github.com>
2025-07-01 11:12:19 -0400

    Delete LibreShelf directory

--- LibreShelf/frontend/src/components/Reader.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams, Navigate } from 'react-router-dom';
-
-export default function Reader() {
-  const { filename } = useParams();
-
-  if (!filename) return <p>Invalid book file.</p>;
-
-  return <Navigate to={`/readium/index.html?epub=/api/read/${filename}`} />;
-}
